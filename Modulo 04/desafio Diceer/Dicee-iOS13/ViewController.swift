4d637bb14a49b73c6abb044e0305a9201d5d15b5
Raul Felippe <felippe.andrade.raul@gmail.com>
2022-05-28 23:56:19 -0300

    array

--- Modulo 04/desafio Diceer/Dicee-iOS13/ViewController.swift
+++ Modulo 04/desafio Diceer/Dicee-iOS13/ViewController.swift
@@ -10,11 +10,19 @@ class ViewController: UIViewController {
         //diceImageView1.image = #imageLiteral(resourceName: "DiceSix")
         //diceImageView1.alpha = 0.5
         
-        let diceArray = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
+        //remover este comentário
+        //        let diceArray = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
+        
+        let diceArray = [
+            UIImage(named: "DiceOne"),
+            UIImage(named: "DiceTwo"),
+            UIImage(named: "DiceThree"),
+            UIImage(named: "DiceFour"),
+            UIImage(named: "DiceFive"),
+            UIImage(named: "DiceSix"),
+        ]
         
         diceImageView1.image = diceArray[Int.random(in: 0...5)]
         diceImageView2.image = diceArray[Int.random(in: 0...5)]
-        
     }
-    
 }
